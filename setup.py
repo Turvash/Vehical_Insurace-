4bfe1376c115edfbaffc5a21815644cd335ed931
Turvash <singhturvash@rediffmail.com>
2025-09-02 02:21:51 +0530

    Project structure ready, venv ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Turvash",
+    author_email="turvashsingh@gmail.com",
+    packages=find_packages()
+)
